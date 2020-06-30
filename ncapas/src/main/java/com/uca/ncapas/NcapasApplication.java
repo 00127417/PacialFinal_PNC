e4fc6b828b4eb842ddf0a551a599489f8d599e30
00120717 <00120717@uca.edu.sv>
2020-06-29 19:56:57 -0600

    First Commit

---
+++ ncapas/src/main/java/com/uca/ncapas/NcapasApplication.java
@@ -0,0 +1,13 @@
+package com.uca.ncapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcapasApplication.class, args);
+	}
+
+}
